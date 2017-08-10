f1076856ca77788feb5f2af75fd7187a09ee7c71
herrinca <charles.a.herring@vanderbilt.edu>
2017-08-10 15:01:47 -0500

    Upload of src

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup( name='pcreode',
+       version='0.1',
+       description='p-Creode algorithm for mapping state transitions',
+       url='https://github.com/herrinca/pCreode',
+       author='Chuck Herring',
+       author_email='charles.a.herring@Vanderbilt.edu',
+       license='MIT',
+       packages=[''],
+       install_requires=[
+           'numpy>=1.11.0',
+           'pandas>=0.17.1',
+           'tsne==0.1.3',
+           'matplotlib',
+           'sklearn',
+           'igraph',
+           'jupyter'],)
+
