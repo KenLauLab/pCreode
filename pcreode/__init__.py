8d0e52cdeccedfad0fb369bef17cd541b7c13afb
Chuck <charles.a.herring@vanderbilt.edu>
2017-08-11 12:46:13 -0500

    fix tsne verison

--- pcreode/__init__.py
+++ pcreode/__init__.py
@@ -1,3 +1,3 @@
 from pcreode import *
 
-__version__ = "0.9"
+__version__ = "0.10"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name='pcreode',
-       version='0.9',
+       version='0.10',
        description='p-Creode algorithm for mapping state transitions',
        url='https://github.com/herrinca/pCreode',
        author='Chuck Herring',
@@ -11,11 +11,11 @@ setup( name='pcreode',
        install_requires=[
            'numpy>=1.11.0',
            'pandas>=0.17.1',
-           'tsne==0.1.7',
+           'pyrex>=9.4.0',
+           'tsne>=0.1.7',
            'matplotlib',
            'sklearn',
            'igraph',
-           'pyrex',
            'jupyter',
        ],
      )
