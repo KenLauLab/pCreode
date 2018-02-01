90d2807b283e02aea4589db6d5a5a0eeb9427b0c
KenLauLab <charles.a.herring@vanderbilt.edu>
2018-02-01 14:33:17 -0600

    tutorial edit

--- pcreode/__init__.py
+++ pcreode/__init__.py
@@ -1,3 +1,3 @@
 from pcreode import *
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name='pcreode',
-       version='1.1.0',
+       version='1.1.1',
        description='p-Creode algorithm for mapping state transitions',
        url='https://github.com/KenLauLab/pCreode',
        author='Chuck Herring',
