68f0e5f2430367ced73c76d5bad73b395669ea17
herrinca <charles.a.herring@vanderbilt.edu>
2018-08-22 16:26:09 -0500

    test.py updated

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import pcreode
+
+file_nm = "../data/synthetic.csv"
+data_raw = pd.read_csv( file_nm, skiprows=[0])
+
+
 
 print "imports passed"
