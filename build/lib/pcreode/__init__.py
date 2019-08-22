5cc62e847ff3dfa30440f5073445e9e834433346
herrinca <chuck.herring@uwa.edu.au>
2019-08-22 09:38:54 +0800

    delete unneeded files

--- build/lib/pcreode/__init__.py
+++
@@ -1,8 +0,0 @@
-#somehow doing the below allows you to access them just by saying 'pcreode'
-from .pcreode import *
-#from pcreode import functions
-
-#also had to remove the "from functions import *" from pcreode.py
-
-__version__ = "2.1.0"
-
